1ca95fdfd8d9aef9c5e50b587893468fddc34f78
ElnazHnia <133989700+ElnazHnia@users.noreply.github.com>
2024-06-20 10:58:36 +0200

    Add files via upload

--- project/tests.sh
+++ project/tests.sh
@@ -1,2 +1,12 @@
+
 #!/bin/bash
-python ../project/tests.py
+
+# python ../project/tests.py
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+echo "Running data processing and statistical analysis..."
+
+# Run the Python script
+python tests.py
